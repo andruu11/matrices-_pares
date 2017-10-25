f2f9d81ce53a503d3736b2667a2d0ef50c01f96a
Jhass11 <jhass11_candia_valdez@outlook.com>
2017-10-25 17:50:02 -0400

    Ejemplo matrices pares

---
+++ matriz pares/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz_pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca cantidad de filas:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca cantidad de columnas:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int[,] matrizb = new int[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        matrizb[i, j] = 0;
+                    }
+                    else
+                    {
+                        matrizb[i, j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(matrizb[i, j] + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
